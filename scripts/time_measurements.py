63d13ef9f97f40f2b23e258e34c0dbadd3c05641
chrisAmbagtsheer <chris.ambagtsheer@wur.nl>
2026-06-24 14:25:23 +0200

    feat: added files to ignore in the .gitignore. Additionally, removed redundant files and cleaned up the repository. Added automatic dependency download for R and Python.

--- scripts/time_measurements.py
+++ scripts/time_measurements.py
@@ -13,11 +13,12 @@ Description: Customers prefer products as fresh as possible, so older
     The data is generated for each run, where the next states are computed
     using the previous state and action. The customer behaviour is taken
     into account within the reward function.
-Usage: python time_measurements.py [file_name]
+Usage: python time_measurements.py [model_output_file] [ctrl_output_file]
     Where:
         python = python 3.0+ (program),
         time_measurements.py = this script,
-        file_name = optional file name
+        model_output_file = optional file name for output models
+        ctrl_output_file = optional file name for output fixed discount control.
 """
 
 
@@ -102,19 +103,76 @@ def create_output(settings, methods, rlkwargs,
                 })
                 pd.DataFrame(results).to_csv(file)
 
+def create_ctrl_output(settings, file = '../data/timemeasurements_control.csv'):
+    '''Create the fixed discount control output of the model.
+
+    :param settings: pd.dataframe. Contains the settings to run the model with.
+    :param file: string. Contains the output file.
+    :return: None
+    '''
+
+    repetitions = 2
+    results = []
+
+    count = 0
+    for M, mu, z in settings:
+        env = Environment(max_shelf_life=M, mu=mu, z=z)
+        for rep in range(repetitions):
+            profit = 0
+            waste = 0
+            fill_rate = 0
+            elapsed_best = 0
+            action_best = 0
+
+            for action in range(env.discount_levels):
+                start = time.perf_counter()
+
+                env.reset()
+                done = False
+                while not done:
+                    _, _, done = env.step(action)
+                if env.get_statistics()['profit'] > profit:
+                    profit = env.get_statistics()['profit']
+                    fill_rate = env.get_statistics()['fill_rate']
+                    waste = env.get_statistics()['waste']
+                    elapsed_best = time.perf_counter() - start
+                    action_best = action
+
+            results.append({
+                'M': M,
+                'mu': mu,
+                'z': z,
+                'method': 'control',
+                'rep': rep + 1,
+                'time': elapsed_best,
+                'action': action_best,
+                'profit': profit,
+                'waste': waste,
+                'fill_rate': fill_rate,
+            })
+
+            print(results[count])
+            pd.DataFrame(results).to_csv(file)
+            count += 1
+
+
 def main():
     """Main function of the module."""
+
+    file = '../data/timemeasurements_check.csv'
+    ctrl_file = '../data/timemeasurements_control.csv'
 
     if len(argv) < 1:
         raise ValueError('Incorrect use of script')
     elif len(argv) == 2:
         file = argv[1]
-    else:
-        file = '../data/timemeasurements_check.csv'
+    elif len(argv) == 3:
+        file = argv[1]
+        ctrl_file = argv[2]
 
     settings, methods, rlkwargs = get_variables()
     create_output(settings, methods, rlkwargs, file)
-
+    create_ctrl_output(settings, ctrl_file)
     print('done')
 
 if __name__ == '__main__':
